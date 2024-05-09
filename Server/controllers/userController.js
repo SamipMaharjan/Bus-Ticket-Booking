25924ef262c677b3d11f7d6209e4c5a1820ea02d
AaryanRatnaTuladhar <aryantuladhar@gmail.com>
2024-05-09 08:53:24 +0545

    added booked trips

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,4 +1,7 @@
+const UpcommingTrips = require("../model/UpcommingTrips");
 const Users = require("../model/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const handleGetAllUsers = async (req, res) => {
   try {
@@ -27,4 +30,53 @@ const deleteUser = async (req, res) => {
     return res.status(500).json({ success: false, message: "Internal Server Error"})
   }
 };
-module.exports = { handleGetAllUsers, deleteUser };
+
+const bookTrip = async (req,res) => {
+  try{
+    const tripId = req.params.id;
+    const result = await UpcommingTrips.findOne({id: tripId}).exec();
+    if (result) {
+      const user = User.find({email: req.email}).exec();
+      // Update a single document
+    User.updateOne({ name: user }, { $set: { name: 'NewCompany' } }, (err, result) => {
+      if (err) {
+          console.error(err);
+      } else {
+          console.log('Updated document:', result);
+      }
+    });
+      user.bookedTrips.push(resutlt);
+      user.save()
+      
+      // const authHeader = req.headers.authorization || req.headers.Authorization;
+      // if (!authHeader.startsWith("Bearer ")) {
+      //   return res
+      //     .status(401)
+      //     .json({ message: "Bearer missing in authorization header." });
+      // }
+    
+      // const token = authHeader.split(" ")[1];
+      
+      // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
+      //   if (err) {
+      //     console.error("\n Error:", err);
+      //     return res.sendStatus(403);
+      //   }
+    
+      //   const foundUser = await Users.findOne({ email: decoded.UserInfo.email }).exec();
+      //   // console.log(foundUser);
+      //   if (!foundUser) {
+      //     console.log("401:", email, "User does not exist");
+      //     return res
+      //       .status(401)
+      //       .json({ error: "Unauthorized: User does not exist." });
+      //   }
+        
+      //   res.status(200).json({"success": foundUser}); // Attach result to req object
+      // });
+    }
+  } catch (err) {
+    
+  }
+}
+module.exports = { handleGetAllUsers, deleteUser, bookTrip };
--- Server/routes/api/users.js
+++ Server/routes/api/users.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
-const { handleGetAllUsers, deleteUser } = require("../../controllers/userController");
+const { verifyJWT } = require("../../middleware/verifyJWT");
+const verifyRoles = require("../../middleware/verifyRoles");
+const ROLES_LIST = require("../../config/roles_list");
+
+const { handleGetAllUsers, deleteUser, bookTrip } = require("../../controllers/userController");
 
 router
     .route("/users")
     .get(handleGetAllUsers)
     .delete(deleteUser);
+
+router.route("/users/bookTrip/:id").post(bookTrip);
 
 module.exports = router;
